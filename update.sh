93194db4e24b49f5bf1e70af9210d6c3ed0fb7c7
jigassa <jigassa@gmail.com>
2021-09-22 16:19:45 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,4 +1,4 @@
 #!/bin/sh.
 #sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
 #sudo rm /home/pi/PX-Box-RPI4-1080p-HLS/update.sh
-sudo wget http://pxslive.ddns.net/PXLOOP.mp4 /home/pi/PXLOOP.mp4
+#sudo wget http://pxslive.ddns.net/PXLOOP.mp4 /home/pi/PXLOOP.mp4
