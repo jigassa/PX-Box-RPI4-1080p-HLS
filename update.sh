26f7ac1a2908bed01c7f64e6e702e878c1e7c8d2
jigassa <jigassa@gmail.com>
2021-09-22 13:46:00 -0500

    Exitoso autoupdate

--- update.sh
+++ update.sh
@@ -1,3 +1,3 @@
 #!/bin/sh.
-sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
-sudo rm /home/pi/PX-Box-RPI4-1080p-HLS/update.sh
+#sudo apt update && sudo apt upgrade -y && sudo apt autoremove -y
+#sudo rm /home/pi/PX-Box-RPI4-1080p-HLS/update.sh
